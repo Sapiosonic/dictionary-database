9b8b4253e5713bc76bd87f2a3c23b9dfe809b5b9
Radamés <radames.idiomas@gmail.com>
2024-01-07 23:49:02 -0300

    create function enter_password

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 people = {"Dave": "joiner", "Bob": "builder"}
 
+
 def intro():
     print("Welcome to the Database\n")
     print("To get access, please enter your password")
     enter_password()
+
+
+def enter_password():
+    password = "123abc"
+    entry_one = input("Enter password")
+    if(len(entrey_one) < 3 and (entry_one != password)):
+      print("Access Denied!")
+    else:
+       print("Access Granted!")
+       data_base()
 
 

42ed5206aa3874fc9deeec963d2ebd869deeec9c
Radamés <radames.idiomas@gmail.com>
2024-01-07 23:46:18 -0300

    create intro function

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 people = {"Dave": "joiner", "Bob": "builder"}
 
+def intro():
+    print("Welcome to the Database\n")
+    print("To get access, please enter your password")
+    enter_password()
+
 
